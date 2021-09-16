2ee7649bce3d6e13391c7cdf3be731548e1308de
Maestro6788 <ensu7928@gmail.com>
2021-09-16 23:35:45 +0900

    feat: API응답 클래스

---
+++ src/main/java/com/bithumb/auth/common/response/ApiResponse.java
@@ -0,0 +1,66 @@
+package com.bithumb.auth.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+	private StatusCode status;
+	private T message;
+	private T data;
+
+	public ApiResponse(StatusCode status, T message) {
+		this.status = status;
+		this.message = message;
+	}
+
+	public ApiResponse(StatusCode status, T message, T data) {
+		this.status = status;
+		this.message = message;
+		this.data = data;
+	}
+
+	public StatusCode getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusCode status) {
+		this.status = status;
+	}
+
+	public T getMessage() {
+		return message;
+	}
+
+	public void setMessage(T message) {
+		this.message = message;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public static ApiResponse responseList(StatusCode status, String message, List data) {
+		return new ApiResponse(status, message, data);
+	}
+
+
+	public static ApiResponse responseError(StatusCode status, Map message) {
+		return new ApiResponse(status, message);
+	}
+
+	public static ApiResponse responseMessage(StatusCode status, String message) {
+		return new ApiResponse(status, message);
+	}
+
+	public static ApiResponse responseData(StatusCode status, String message, Object data) {
+		return new ApiResponse(status, message, data);
+	}
+
+}
