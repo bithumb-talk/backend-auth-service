b9a4cffbc316c8ada3e0b598cab0ba6e6fc15d99
Maestro6788 <ensu7928@gmail.com>
2021-09-23 20:26:17 +0900

    feat: Auth Controller 응답 DTO

---
+++ src/main/java/com/bithumb/auth/auth/controller/dto/TokenResponseDto.java
@@ -0,0 +1,33 @@
+package com.bithumb.auth.auth.controller.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Builder
+public class TokenResponseDto {
+
+  private long id;
+  private String grantType;
+  private String accessToken;
+  private String refreshToken;
+  private Long accessTokenExpiresIn;
+
+  @Builder
+  public TokenResponseDto(long id, String grantType, String accessToken,
+      String refreshToken, Long accessTokenExpiresIn) {
+    this.id = id;
+    this.grantType = grantType;
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+    this.accessTokenExpiresIn = accessTokenExpiresIn;
+  }
+
+
+  public static TokenResponseDto of(long id, TokenDto tokenDto) {
+    return new TokenResponseDto(id, tokenDto.getGrantType(), tokenDto.getAccessToken(),
+        tokenDto.getRefreshToken(), tokenDto.getAccessTokenExpiresIn());
+  }
+}
