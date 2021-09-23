c5683fe940778172dd380d23e8c8d2e1b3f54fa2
Maestro6788 <ensu7928@gmail.com>
2021-09-23 22:42:41 +0900

    feat: RefreshToken / DTO

---
+++ src/main/java/com/bithumb/auth/auth/controller/dto/TokenRequestDto.java
@@ -0,0 +1,11 @@
+package com.bithumb.auth.auth.controller.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenRequestDto {
+    private String accessToken;
+    private String refreshToken;
+}
