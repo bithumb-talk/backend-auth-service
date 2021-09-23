21239c564368776934c6b795d8bde8b063f19bea
Maestro6788 <ensu7928@gmail.com>
2021-09-24 01:39:10 +0900

    feat: 간단한 코드 style 변경

--- src/main/java/com/bithumb/auth/config/JwtSecurityConfig.java
+++ src/main/java/com/bithumb/auth/config/JwtSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.bithumb.auth.security.AuthFilter;
+import com.bithumb.auth.security.authentication.AuthFilter;
 import com.bithumb.auth.security.jwt.TokenProvider;
 
 import lombok.RequiredArgsConstructor;
