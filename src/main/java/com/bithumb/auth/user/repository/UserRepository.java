48e62d947ba8f11560f1c9caeb523a53193a0559
Maestro6788 <ensu7928@gmail.com>
2021-09-23 20:26:34 +0900

    feat: User Repository

---
+++ src/main/java/com/bithumb/auth/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.bithumb.auth.user.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bithumb.auth.user.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByNickname(String nickname);
+    Optional<User> findByUserId(String userId);
+    boolean existsByUserId(String userId);
+
+}
