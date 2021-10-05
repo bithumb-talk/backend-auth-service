0566220eadd3978fdb04e137b425a4d7f65591d9
Maestro6788 <ensu7928@gmail.com>
2021-10-06 03:54:40 +0900

    refactor: Entity 칼럼추가

--- src/main/java/com/bithumb/auth/user/domain/User.java
+++ src/main/java/com/bithumb/auth/user/domain/User.java
@@ -24,70 +24,44 @@ import lombok.ToString;
 @Builder
 public class User {
 
-    @Id
-    @Column(name = "user_no")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@Column(name = "user_no")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(name = "user_id", nullable = false)
-    private String userId;
+	@Column(name = "user_id", nullable = false)
+	private String userId;
 
-    @Column(name = "user_password", nullable = false)
-    private String password;
+	@Column(name = "user_password", nullable = false)
+	private String password;
 
-    @Column(name = "user_nickname", nullable = false)
-    private String nickname;
+	@Column(name = "user_nickname", nullable = false)
+	private String nickname;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "user_authority", nullable = false)
-    private Authority authority;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "user_authority", nullable = false)
+	private Authority authority;
 
-    @Column(name = "device_token", nullable = true)
-    private String deviceToken;
+	@Column(name = "user_profile_url")
+	private String profileUrl;
 
-    public void changePassword(String password) {
-        this.password = password;
-    }
+	@Column(name = "device_token")
+	private String deviceToken;
 
-    public void changeNickname(String nickname) {
-        this.nickname = nickname;
-    }
+	public void changePassword(String password) {
+		this.password = password;
+	}
 
-    public void changeDeviceToken(String deviceToken) {
-        this.deviceToken = deviceToken;
-    }
+	public void changeNickname(String nickname) {
+		this.nickname = nickname;
+	}
 
+	public void changeProfileUrl(String profileUrl) {
+		this.profileUrl = profileUrl;
+	}
 
-    @Builder
-    public User(Long id, String userId, String password, String nickname,
-        Authority authority) {
-        this.id = id;
-        this.userId = userId;
-        this.password = password;
-        this.nickname = nickname;
-        this.authority = authority;
-    }
-
-    @Builder
-    public User(String userId, String password, String nickname,
-        Authority authority) {
-        this.userId = userId;
-        this.password = password;
-        this.nickname = nickname;
-        this.authority = authority;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-            "id=" + id +
-            ", userId='" + userId + '\'' +
-            ", password='" + password + '\'' +
-            ", nickname='" + nickname + '\'' +
-            ", authority=" + authority +
-            '}';
-    }
-
-
+	public void changeDeviceToken(String deviceToken) {
+		this.deviceToken = deviceToken;
+	}
 
 }
