12e7359272cdef366cb9e6b993bfd9ed830e9a8f
Maestro6788 <ensu7928@gmail.com>
2021-10-06 04:33:58 +0900

    feat: 마이페이지 게시판 좋아요 추가 / Repository

---
+++ src/main/java/com/bithumb/auth/board/repository/BoardRepository.java
@@ -0,0 +1,25 @@
+package com.bithumb.auth.board.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.bithumb.auth.board.entity.Board;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+	@Query("SELECT"
+		+ " CASE "
+		+ "WHEN COUNT(b) > 0 "
+		+ "THEN true "
+		+ "ELSE false END "
+		+ "FROM Board b WHERE b.userId = :userId and b.boardId = :boardId")
+	boolean checkAlreadyExist(@Param("userId") Long userId ,@Param("boardId") Long boardId);
+
+
+}
