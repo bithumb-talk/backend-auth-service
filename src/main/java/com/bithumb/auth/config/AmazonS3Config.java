5e6308aca917ca02933c17d95d6d73e91e33a4a0
Maestro6788 <ensu7928@gmail.com>
2021-10-13 11:03:37 +0900

    feat: S3 IAM 삭제

--- src/main/java/com/bithumb/auth/config/AmazonS3Config.java
+++ src/main/java/com/bithumb/auth/config/AmazonS3Config.java
@@ -12,21 +12,21 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 @Configuration
 public class AmazonS3Config {
 
-	@Value("${cloud.aws.credentials.access-key}")
-	private String accessKey;
-
-	@Value("${cloud.aws.credentials.secret-key}")
-	private String secretKey;
+	// @Value("${cloud.aws.credentials.access-key}")
+	// private String accessKey;
+	//
+	// @Value("${cloud.aws.credentials.secret-key}")
+	// private String secretKey;
 
 	@Value("${cloud.aws.region.static}")
 	private String region;
 
 	@Bean
 	public AmazonS3Client amazonS3Client() {
-		BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+		//BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
 		return (AmazonS3Client)AmazonS3ClientBuilder.standard()
 			.withRegion(region)
-			.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+			//.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
 			.build();
 	}
 }
